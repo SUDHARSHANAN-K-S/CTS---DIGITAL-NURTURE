38be143cfeb5e22dc1cbebdef1ca50c3b77d3cd5
SUDHARSHANAN-K-S <sudharshanansathishkumar@gmail.com>
2026-07-20 16:38:25 +0530

    Describe your changes

--- WEEK - 1/Design Patterns and Principles/ImplementingTheSingletonPattern/SingletonTest.java
+++ WEEK - 1/Design Patterns and Principles/ImplementingTheSingletonPattern/SingletonTest.java
@@ -1,12 +1,12 @@
 public class SingletonTest {
     public static void main(String[] args) {
-        Logger logger1 = Logger.getInstance();
-        Logger logger2 = Logger.getInstance();
+        Logger log1 = Logger.getInstance();
+        Logger log2 = Logger.getInstance();
 
-        logger1.log("First log message");
-        logger2.log("Second log message");
+        log1.log("First log message");
+        log2.log("Second log message");
 
-        if (logger1 == logger2) {
+        if (log1 == log2) {
             System.out.println("Only one Logger instance is created.");
         } else {
             System.out.println("Multiple Logger instances are created.");
